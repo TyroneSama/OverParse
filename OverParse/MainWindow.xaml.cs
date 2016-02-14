5c2aa307f4c4314f78fcde60d1a8fc64d4183953
AJ Martinez <hallowedstream@gmail.com>
2016-02-14 16:47:05 -0700

    Handle situations where the OverParse folder is read-only (FUCK YOU MICROSOFT)

--- OverParse/MainWindow.xaml.cs
+++ OverParse/MainWindow.xaml.cs
@@ -42,7 +42,12 @@ namespace OverParse
                 Properties.Settings.Default.UpgradeRequired = false;
             }
 
-            Directory.CreateDirectory("logs");
+            try { Directory.CreateDirectory("logs"); }
+            catch {
+                MessageBox.Show("OverParse doesn't have write access to its folder, and won't be able to save logs. This usually happens when you run it from Program Files.\n\nThis is a Windows restriction, and unfortunately I can't do anything about it.\n\nPlease run OverParse as administrator, or move it somewhere else. Sorry for the inconvenience!");
+                Application.Current.Shutdown();
+            }
+
             this.Top = Properties.Settings.Default.Top;
             this.Left = Properties.Settings.Default.Left;
             this.Height = Properties.Settings.Default.Height;
