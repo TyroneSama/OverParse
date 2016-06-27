1746b70b3a2f5c88aba761b9829a8000f5bd2ef7
Alam Arias <Alam.GBC+git@gmail.com>
2016-06-27 12:18:06 -0400

    Revert "Log: Renamed Resources files from LogResources to just Log"
    This reverts commit a8425eee9ca63c17595c18a2df7667c1003c5a13.

--- OverParse/LogResources.Designer.cs
+++ OverParse/LogResources.Designer.cs
@@ -22,14 +22,14 @@ namespace OverParse {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Log {
+    internal class LogResources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Log() {
+        internal LogResources() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace OverParse {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("OverParse.Log", typeof(Log).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("OverParse.LogResources", typeof(LogResources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
